476f6e95e0a4a7993efc06c71a80395dce3d0503
tobsval <75995708+tobsval@users.noreply.github.com>
2021-03-25 22:11:31 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,6 +10,4 @@ terraform {
 # Configure AWS provider - credentials are exported through the 01-aws-creds.sh script
 provider "aws" {
     region     = "us-east-1"
-    access_key = "AKIASEIQJEMOBDBAJMQA"
-    secret_key = "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"
 }
